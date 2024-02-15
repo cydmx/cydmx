fde20e112b3aebaeb6932e450c72f969e9b0bc0f
cydmx <160136518+cydmx@users.noreply.github.com>
2024-02-15 23:45:45 +0800

    Delete QX-qu-guang-gao directory

--- QX-qu-guang-gao/FL-fan-qie-xiao-shuo.js
+++
@@ -1,6 +0,0 @@
-ip-cidr, 49.71.37.101/32, reject
-ip-cidr, 117.71.105.23/32, reject
-ip-cidr, 218.94.207.205/32, reject
-ip-cidr, 117.92.229.188/32, reject
-ip-cidr, 101.36.166.16/32, reject
-ip-cidr, 180.96.2.114/32, reject
